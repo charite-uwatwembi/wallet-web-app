ffe2a6567304a5cc1328ad939a67251edad6f159
charite-uwatwembi <c.uwatwembi@alustudent.com>
2025-01-19 23:47:07 +0200

    webApp

--- wallet-backend/server.js
+++ wallet-backend/server.js
@@ -1,7 +1,17 @@
 const app = require("./app");
+const budgetRoutes = require("./routes/budgetRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+
+app.use("/api/budgets", budgetRoutes);
+app.use("/api/reports", reportRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
+
+
